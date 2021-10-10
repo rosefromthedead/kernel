ceeae34b75f6519053dc07ff4ab0ed5c8e1518d2
Rose Hudson <cv21874@bristol.ac.uk>
2021-10-10 19:35:12 +0100

    Flake fixes - rustup hook and flake input package confusion

--- flake.nix
+++ flake.nix
@@ -5,12 +5,18 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, flake-utils, mkarm64image }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in
-    {
-      devShell = pkgs.pkgsCross.aarch64-multiplatform.mkShell {
-        depsBuildBuild = with pkgs; [ cacert rustup gcc gdb mkarm64image qemu ];
-        preUnpack = "rustup component add rust-src rust-analyzer";
-      };
-    }
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      triple = (nixpkgs.lib.systems.elaborate system).config;
+    in
+      {
+        devShell = pkgs.pkgsCross.aarch64-multiplatform.mkShell {
+          depsBuildBuild = with pkgs; [ cacert rustup gcc gdb mkarm64image.packages.${system}.mkarm64image qemu ];
+          shellHook = ''
+            rustup component add rust-src rust-analyzer-preview
+            export PATH=$PATH:~/.rustup/toolchains/${pkgs.lib.readFile ./rust-toolchain}-${triple}/bin/
+          '';
+        };
+      }
   );
 }
