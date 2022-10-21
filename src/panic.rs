3be44ff3838878fd1be6f038a6473428ff4e041f
Rose Hudson <cv21874@bristol.ac.uk>
2022-10-21 23:47:26 +0100

    print column in panic info

--- src/panic.rs
+++ src/panic.rs
@@ -6,7 +6,12 @@ pub extern "C" fn rust_eh_personality() {}
 #[no_mangle]
 pub extern "C" fn rust_begin_unwind(info: &core::panic::PanicInfo) -> ! {
     match info.location() {
-        Some(loc) => tracing::error!(file=loc.file(), line=loc.line(), "panic encountered"),
+        Some(loc) => tracing::error!(
+            file=loc.file(),
+            line=loc.line(),
+            col=loc.column(),
+            "panic encountered",
+        ),
         None => tracing::error!("panic encountered"),
     }
     if let Some(message) = info.message() {
