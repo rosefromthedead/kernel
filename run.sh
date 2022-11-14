c95b8390c85e14cc2c6c28c857296220f1fccc7a
Rose Hudson <cv21874@bristol.ac.uk>
2022-11-14 14:30:34 +0000

    create cargo workspace for target/ sharing

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ cd init
 cargo build --target aarch64-unknown-none -Zbuild-std=core,alloc
 cd ..
 
-qemu-system-aarch64 -M virt -cpu cortex-a53 -m 1g -nographic -kernel build/kernel.ub -initrd init/target/aarch64-unknown-none/debug/init $@
+qemu-system-aarch64 -M virt -cpu cortex-a53 -m 1g -nographic -kernel build/kernel.ub -initrd target/aarch64-unknown-none/debug/init $@
