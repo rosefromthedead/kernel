266f780435768d4e0ca5ab68f363eda2c6c6a88d
Rose Hudson <rose@krx.sh>
2024-02-25 20:46:46 +0000

    allow building on aarch64

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+toolchain_prefix=aarch64-unknown-linux-gnu-
+if [ $(uname -m) = aarch64 ]; then
+    toolchain_prefix=
+fi
+
 # use gnu ld - default lld gives some weird relocation error and i don't want to deal with it
-RUSTFLAGS="-Clinker=aarch64-unknown-linux-gnu-gcc -Clink-arg=-Tkernel/aarch64.ld" \
+RUSTFLAGS="-Clinker=${toolchain_prefix}gcc -Clink-arg=-Tkernel/aarch64.ld -Clink-arg=-nostdlib" \
     cargo build --target aarch64-unknown-none --features build-asm -Zbuild-std=core,alloc
 
-aarch64-unknown-linux-gnu-objcopy -S -O binary ../target/aarch64-unknown-none/debug/kernel ../build/kernel.bin
+${toolchain_prefix}objcopy -S -O binary ../target/aarch64-unknown-none/debug/kernel ../build/kernel.bin
 mkarm64image --overwrite --entry-point=0x200000 ../build/kernel.bin ../build/kernel.ub
